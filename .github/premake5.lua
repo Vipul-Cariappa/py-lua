793713456fbd5b7e1f3952f98810e157c3e52522
Vipul Cariappa <vipulcariappa@gmail.com>
2021-10-10 05:00:00 +0000

    updated workflow

--- .github/premake5.lua
+++ .github/premake5.lua
@@ -6,11 +6,11 @@ project "pylua"
    language "C"
    targetdir "bin"
 
-   includedirs { "src/header", "/usr/include/python3.9", "/usr/include/lua5.4" }
+   includedirs { "src/header", "/usr/include/python3.8", "/usr/include/lua5.4" }
    files { "src/**.h", "src/**.c" }
 
    filter { "system:linux" }
-      links { "python3.9", "lua5.4" }
+      links { "python3.8", "lua" }
 
    filter "configurations:Debug"
       defines { "DEBUG" }
