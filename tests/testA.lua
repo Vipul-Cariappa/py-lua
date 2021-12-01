20d2c8526e84001ce57fe1e20f4050be2d460482
Vipul Cariappa <vipulcariappa@gmail.com>
2021-12-01 11:02:10 +0530

    Added test cases for python bindings

--- tests/testA.lua
+++ tests/testA.lua
@@ -1,4 +1,19 @@
+lu = require('luaunit')
+
+
 string = "Lua"
 int = 121
 boolean = false
 list = {0, 1, 2, 3, 4, 5}
+
+
+function return121()
+    return 121
+end
+
+function get_values(a, b, c, d)
+    lu.assertEquals(a, 121)
+    lu.assertEquals(b, 1.5)
+    lu.assertEquals(c, false)
+    lu.assertEquals(d, "Python")
+end
--- tests/testA.py
+++ tests/testA.py
@@ -1,7 +1,7 @@
 import unittest
 import pylua
 
-lua_module = pylua.LuaLoad("testA")
+lua_module = pylua.LuaLoad("testA.lua")
 
 class TestBasic(unittest.TestCase):
     def test_simple_types(self):
@@ -9,9 +9,15 @@ class TestBasic(unittest.TestCase):
         self.assertEqual(lua_module.int, 121)
         self.assertFalse(lua_module.boolean)
 
+    @unittest.skip("Convertion to list or iteration not yet implemented")
     def test_complex_types(self):
         for i in enumerate(lua_module.list):
             self.assertEqual(e, i)
+
+    def test_function_calls(self):
+        self.assertEqual(lua_module.return121(), 121)
+        lua_module.get_values(121, 1.5, False, "Python")
+
 
 if __name__ == "__main__":
     unittest.main()
